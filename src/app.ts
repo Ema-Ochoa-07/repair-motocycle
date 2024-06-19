3e3bef6f7f50776954d3166bff40884db1ec17b2
Enmanuel Ochoa <163493441+Ema-Ochoa-07@users.noreply.github.com>
2024-06-18 19:09:58 -0500

    Se vinculó el ORM de BD Postgres con autenticación de una base datos prueba videogame

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
+import "reflect-metadata"
 import { AppRouter } from "./presentation/routes"
 import { Server } from "./presentation/server"
+import { PostgresDatabase } from "./data"
 
 
 (async() =>{
@@ -7,6 +9,20 @@ import { Server } from "./presentation/server"
 })()
 
 async function main () {
+
+    //CAMBIAR POR LA NUEVA BD
+
+    const postgres = new PostgresDatabase({
+        host: 'ep-steep-limit-a55j8wuz.us-east-2.aws.neon.tech',
+        port:5432,
+        username: 'videogamedb_owner',
+        password: 'okpN4EvH8yzn',
+        database: 'videogamedb'
+
+    })
+
+    await postgres.connect()
+
     const server = new Server({
         port:3000,
         routes: AppRouter.routes
---
+++ src/data/postgres/postgres-database.ts
@@ -0,0 +1,43 @@
+import { DataSource } from "typeorm";
+
+interface Options {
+    host: string
+    port: number
+    username: string
+    password:string
+    database:string
+}
+
+export class PostgresDatabase{
+
+    private datasource: DataSource
+
+    constructor(option: Options){
+        this.datasource = new DataSource({
+            type:'postgres',
+            host: option.host,
+            port: option.port,
+            username: option.username,
+            password: option.password,
+            database: option.database,
+            //entities
+
+            synchronize: true,
+            ssl:{
+                rejectUnauthorized: false
+            }
+        })
+    } 
+
+    async connect(){
+        try {
+            await this.datasource.initialize()
+            console.log('Connected to database 👌')
+            
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+}
+
