c74e0ec6d9acce7d16c2687f54ecc81d665fc090
Enmanuel Ochoa <163493441+Ema-Ochoa-07@users.noreply.github.com>
2024-07-09 15:41:02 -0500

    Add config of CORS

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,6 +1,7 @@
 import express, { Router } from 'express'
 import cors from 'cors'
 import helmet from "helmet";
+import { CallTracker } from 'assert';
 // import
 
 interface Options{
@@ -12,6 +13,7 @@ export class Server {
     public readonly app = express()
     private readonly port: number
     private readonly routes: Router
+    private readonly aceptedOrigin: string[] = ['http://localhost:5173']
 
     constructor(options: Options){
         this.port = options.port
@@ -22,7 +24,19 @@ export class Server {
         //*Utilizar el Middelware Use
         this.app.use( express.json() )
         this.app.use( express.urlencoded({ extended: true }) )
-        this.app.use( cors ) //Medida de seguridadd que otros sitios accedan al servidor
+        this.app.use( cors ({
+            origin: (origin, callback) => {
+
+                if(!origin){
+                    return callback(null, true)
+                }
+
+                if(this.aceptedOrigin.includes(origin!)){
+                    return callback(null, true)
+                }
+                return callback( new Error ('Not allowed by CORS'))
+            }
+        }))
         this.app.use( helmet() ) // otras medidas de seguridad en los headers
         // app.use(hpp())
 
